32874aca3d7e98bfcd4b18fd77353394fa5315ee
Mikk Margus Möll <mikk@cert.ee>
2023-03-21 13:08:21 +0200

    working results

--- params.py
+++ params.py
@@ -15,7 +15,7 @@ filename = os.path.dirname(__file__) + '/out.json'
 with open(filename) as fd:
     definitions = json.load(fd)
 
-current_arch = 'linux-amd64'  # TODO
+current_arch = 'windows-amd64'  # TODO
 
 typemap = definitions['TypeMap']['all']
 typemap.update(definitions['TypeMap'][current_arch])
@@ -45,6 +45,12 @@ slice.add(int_t(), ptr_size, 'cap', None)
 iface = data.StructureDataType('go_iface', 0)
 iface.add(ptr(data.Undefined1DataType()), ptr_size, 'type_ptr', None)
 iface.add(ptr(data.Undefined1DataType()), ptr_size, 'impl_ptr', None)
+
+chan = data.StructureDataType('go_chan', 0)
+chan.add(ptr(data.Undefined1DataType()), ptr_size, 'ptr', None)
+
+map = data.StructureDataType('go_map', 0)
+map.add(ptr(data.Undefined1DataType()), ptr_size, 'ptr', None)
 
 integer_registers = ['RAX', 'RBX', 'RCX', 'RDI', 'RSI', 'R8', 'R9', 'R10', 'R11']
 float_registers = ['XMM{}'.format(i) for i in range(15)]
@@ -81,8 +87,8 @@ type_map = {
     'error': lambda: iface,
     'code': data.Undefined1DataType,  # TODO something better here?
     # probably don't care about the internals of these
-    'chan': lambda: ptr(data.Undefined1DataType()),
-    'map': lambda: ptr(data.Undefined1DataType()),
+    'chan': lambda: chan,
+    'map': lambda: map,
 }
 
 
@@ -97,46 +103,58 @@ def get_type(s):
 
     return type_map[s]()
 
-# TODO
-# https://ghidra.re/ghidra_docs/api/ghidra/program/model/listing/VariableStorage.html
-# https://ghidra.re/ghidra_docs/api/ghidra/program/model/listing/ParameterImpl.html
-# https://ghidra.re/ghidra_docs/api/ghidra/program/model/data/FunctionDefinitionDataType.html
-# https://ghidra.re/ghidra_docs/api/ghidra/app/cmd/function/ApplyFunctionSignatureCmd.html
-# VariableStorage from registers
-# ParameterImpl from above VariableStorage + datatype
-# create FunctionDefinitionDataType with name, populate arguments with setArguments()
-# apply return type on FunctionDefinitionDataType via setReturnType
-# (TODO!!! also need to semi-dynamically generate return types for multi-valued returns)
-# apply signature to function via ApplyFunctionSignatureCmd
-#
-# nvm FunctionDB.replaceParameters()
 
+dynamic_type_map = {}
+
+
+def get_dynamic_type(types):
+    name = '_'.join(chain(['go_dynamic'], types))
+    if name in dynamic_type_map:
+        return dynamic_type_map[name]
+
+    t = data.StructureDataType(name, 0)
+    for i, typename in enumerate(types):
+        element_type = get_type(typename)
+        t.add(element_type, element_type.getLength(), 'elem_{}'.format(i), None)
+
+    dynamic_type_map[name] = t
+    return t
+
+# TODO!!! also need to semi-dynamically generate return types for multi-valued returns
 
 def functions_iter():
     func = getFirstFunction()
     while func is not None:
         yield func
-        func  = getFunctionAfter(func)
+        func = getFunctionAfter(func)
 
 
-def assign_registers(registers, length):
-    registers = registers[:]
-
+def assign_registers(int_registers, float_registers, types):
+    int_registers = int_registers[::-1]
+    float_registers = float_registers[::-1]
     out = []
-    while registers:
-        reg = registers[0]
-        reg_len = reg.getBitLength() >> 3
-        if reg_len > length:
-            registers[0] = reg.getChildRegisters()[0]
-            continue
 
-        out.append(reg)
-        registers = registers[1:]
-        length -= reg_len
-        if not length:
-            return out
+    for t in types:
+        t_len = t.getLength()
+        while t_len:
+            if isinstance(t, data.AbstractFloatDataType):
+                registers = float_registers
+            else:
+                registers = int_registers
 
-    return None
+            if len(registers) == 0:
+                return None
+
+            reg = registers.pop()
+            reg_len = reg.getBitLength() >> 3
+            if reg_len <= t_len:
+                out.append(reg)
+                t_len -= reg_len
+            else:
+                registers.append(reg.getChildRegisters()[0])
+
+    return out
+
 
 def get_params(param_types):
     if 'struct' in param_types:
@@ -151,18 +169,19 @@ def get_params(param_types):
 
     for param in param_types:
         datatype = get_type(param)
-        datatype_len = datatype.getLength()
 
-        if isinstance(datatype, data.AbstractFloatDataType):
-            remaining_registers = remaining_float_registers
-        else:
-            remaining_registers = remaining_int_registers
+        types = recursive_struct_unpack(datatype)
 
-        assigned = assign_registers(remaining_registers, datatype_len)
-            if assigned is None:
-                return None
+        assigned = assign_registers(remaining_int_registers, remaining_float_registers, types)
+        if assigned is None:
+            return None
 
-        remaining_registers[:len(assigned)] = []
+        # TODO amd64_specific
+        float_reg_num = sum(1 for reg in assigned if reg.getTypeFlags() & reg.TYPE_VECTOR)
+        int_reg_num = len(assigned) - float_reg_num
+
+        remaining_int_registers = remaining_int_registers[int_reg_num:]
+        remaining_float_registers = remaining_float_registers[float_reg_num:]
 
         storage = VariableStorage(currentProgram, *assigned)
 
@@ -176,13 +195,47 @@ def get_params(param_types):
     return result_params
 
 
+def get_results(result_types):
+    if 'struct' in result_types:
+        return None
+
+    if len(result_types) == 0:
+        return data.VoidDataType(), VariableStorage.VOID_STORAGE
+
+    remaining_int_registers, remaining_float_registers = (
+        [registers[name] for name in reglist]
+        for reglist in (integer_registers, float_registers)
+    )
+
+    if len(result_types) == 1:
+        datatype = get_type(result_types[0])
+    else:
+        datatype = get_dynamic_type(result_types)
+
+    types = recursive_struct_unpack(datatype)
+    assigned = assign_registers(remaining_int_registers, remaining_float_registers, types)
+    if assigned is None:
+        return None
+
+    storage = VariableStorage(currentProgram, *assigned)
+
+    return datatype, storage
+
+
+def recursive_struct_unpack(datatype):
+    if not isinstance(datatype, data.StructureDataType):
+        yield datatype
+        return
+
+    for component in datatype.getDefinedComponents():
+        # no yield from in py2
+        for v in recursive_struct_unpack(component):
+            yield v
+
+
 def main():
     for func in functions_iter():
-        try:
-            name = func.name.encode()
-        except UnicodeEncodeError:
-            continue
-        signature = funcmap.get(name)
+        signature = funcmap.get(func.name)
         if signature is None:
             continue
 
@@ -190,8 +243,10 @@ def main():
         if arguments is not None:
             func.replaceParameters(FunctionUpdateType.CUSTOM_STORAGE, True, SourceType.USER_DEFINED, *arguments)
 
-
-        # TODO setReturn
+        results = get_results(signature['Results'])
+        if results is not None:
+            datatype, storage = results
+            func.setReturn(datatype, storage, SourceType.USER_DEFINED)
 
 
 main()
