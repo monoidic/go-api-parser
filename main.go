10ec281d6fbbf34c260f2344265867c3c3146adf
Mikk Margus Möll <mikk.margus.moll@eesti.ee>
2023-04-03 01:16:43 +0300

    still WIP Go source code parsing

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"go/parser"
 	"go/token"
 	"go/types"
+	"log"
 	"os"
 	"path/filepath"
 	"sort"
@@ -15,15 +16,18 @@ import (
 	"golang.org/x/tools/go/packages"
 )
 
-// TODO would just parsing the Go source code directly be better...?
-// it'd handle a bunch of runtime functions and unexported functions/methods as well,
-// plus make potentially extending this for extracting function signatures from known code easier,
-// for all that's worth
+/* TODO parse:
+ * type aliases (add to Aliases map) (TODO need separate methods for these? prolly not)
+ * types w/ non-struct underlying type (add to Types map w/ underlying type)
+ * types w/ struct underlying type (add to Structs map)
+ * interfaces (???)
+ * methods (parse while parsing structs, add to Funcs map w/ proper name and receiver as arg;
+	        ignore interface methods + methods inherited from embedded fields)
+ * functions (add to Funcs map)
+*/
 
 func getBuildConstraints() map[string]map[string]bool {
-	out := make(map[string]map[string]bool)
-
-	for _, architecture := range []string{
+	architectures := []string{
 		//"darwin-386",
 		//"darwin-386-cgo",
 		"darwin-amd64",
@@ -60,9 +64,14 @@ func getBuildConstraints() map[string]map[string]bool {
 		"openbsd-amd64-cgo",
 		"windows-386",
 		"windows-amd64",
-	} {
-		archMap := make(map[string]bool)
-		for _, tag := range strings.Split(architecture, "-") {
+	}
+
+	out := make(map[string]map[string]bool, len(architectures))
+
+	for _, architecture := range architectures {
+		split := strings.Split(architecture, "-")
+		archMap := make(map[string]bool, len(split))
+		for _, tag := range split {
 			archMap[tag] = true
 		}
 		out[architecture] = archMap
@@ -75,7 +84,7 @@ var buildConstraints = getBuildConstraints()
 
 func check(err error) {
 	if err != nil {
-		panic(err)
+		log.Panicln(err)
 	}
 }
 
@@ -84,21 +93,22 @@ func check1[T any](arg1 T, err error) T {
 	return arg1
 }
 
-var filteredNames = map[string]bool{
+var filteredDirs = map[string]bool{
 	"testdata": true,
 	"vendor":   true,
-	"internal": true,
-	"arena":    true,
 	"cmd":      true,
+	".git":     true,
 }
 
 func walkTreeDirs(ch chan<- string, root string) {
+	ch <- root
+
 	var dirNames []string
 
 	for _, dir := range check1(os.ReadDir(root)) {
 		if dir.IsDir() {
 			dirName := dir.Name()
-			if !filteredNames[dirName] {
+			if !filteredDirs[dirName] {
 				dirNames = append(dirNames, filepath.Join(root, dir.Name()))
 			}
 		}
@@ -107,7 +117,6 @@ func walkTreeDirs(ch chan<- string, root string) {
 	sort.Strings(dirNames)
 
 	for _, path := range dirNames {
-		ch <- path
 		walkTreeDirs(ch, path)
 	}
 }
@@ -160,9 +169,6 @@ var knownArch = map[string]bool{
 	"wasm":        true,
 }
 
-// https://groups.google.com/g/golang-nuts/c/tJSUNwyayis
-// drop everything below and just move to go/types? what about build constraints?
-
 func getFilenameBuildTags(filePath string) (goos, goarch string) {
 	fileName := filepath.Base(filePath)
 	fileName = fileName[:len(fileName)-3]             // remove .go
@@ -172,13 +178,14 @@ func getFilenameBuildTags(filePath string) (goos, goarch string) {
 	}
 
 	// get last two _-seperated elements
-	if tagsStart := len(potentialTags) - 2; tagsStart > 0 {
-		potentialTags = potentialTags[tagsStart:]
+	if tagsNum := len(potentialTags); tagsNum > 2 {
+		potentialTags = potentialTags[tagsNum-2:]
 	}
 
 	//*_GOOS
 	//*_GOARCH
 	//*_GOOS_GOARCH
+	//*/
 
 	last := potentialTags[len(potentialTags)-1]
 	if knownOS[last] {
@@ -189,8 +196,7 @@ func getFilenameBuildTags(filePath string) (goos, goarch string) {
 		goarch = last
 	}
 	if len(potentialTags) == 2 {
-		first := potentialTags[0]
-		if knownOS[first] {
+		if first := potentialTags[0]; knownOS[first] {
 			goos = first
 		}
 	}
@@ -243,32 +249,26 @@ func getEnv(arch string) []string {
 	return out
 }
 
+func parseDiscardFuncBody(fset *token.FileSet, filename string, src []byte) (*ast.File, error) {
+	f, err := parser.ParseFile(fset, filename, src, parser.AllErrors)
+
+	for _, decl := range f.Decls {
+		if funcDecl, ok := decl.(*ast.FuncDecl); ok {
+			funcDecl.Body = nil
+		}
+	}
+
+	return f, err
+}
+
 func filterPkg(pkg *ast.Package, path string) map[string]*packages.Package {
 	// map<architectureString, map<filePath, astFile>>
 	archMatches := map[string]bool{"all": true}
 
-	/*
-		conf := types.Config{
-			//Importer: importer.Default(),
-			//Importer: importer.ForCompiler(token.NewFileSet(), "source", nil),
-			//Goversion: "go1.20",
-			IgnoreFuncBodies: true,
-			//FakeImportC: true,
-		}
-	*/
 	conf := packages.Config{
-		// probably only need these (TODO check)
-		Mode: packages.NeedTypes | packages.NeedDeps | packages.NeedImports,
-		/*
-			Mode: packages.NeedName | packages.NeedFiles | packages.NeedCompiledGoFiles |
-				packages.NeedImports | packages.NeedDeps | packages.NeedExportFile |
-				packages.NeedTypes | packages.NeedSyntax | packages.NeedTypesInfo |
-				packages.NeedTypesSizes | packages.NeedModule |
-				packages.NeedEmbedFiles | packages.NeedEmbedPatterns,
-		*/
-		Dir: os.Args[1],
-		// use ParseFile to discard function bodies?
-		// parallel parsing of separate packages in pipeline?
+		Mode:      packages.NeedTypes | packages.NeedDeps | packages.NeedImports,
+		Dir:       os.Args[1],
+		ParseFile: parseDiscardFuncBody,
 	}
 
 	for filePath, fileObj := range pkg.Files {
@@ -317,40 +317,284 @@ func filterPkg(pkg *ast.Package, path string) map[string]*packages.Package {
 	return out
 }
 
+type namedType struct {
+	Name     string
+	DataType string
+}
+
+func tupleCmp(x, y []namedType) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i, xEl := range x {
+		if yEl := y[i]; xEl != yEl {
+			return false
+		}
+	}
+	return true
+}
+
+type funcData struct {
+	Params  []namedType
+	Results []namedType
+}
+
+func (x funcData) equals(yI equalsI) bool {
+	y := yI.(funcData)
+	return len(x.Params) == len(y.Params) &&
+		len(x.Results) == len(y.Results) &&
+		tupleCmp(x.Params, y.Params) &&
+		tupleCmp(x.Results, y.Results)
+}
+
+type typeData struct {
+	Underlying string
+}
+
+func (x typeData) equals(yI equalsI) bool {
+	return x == yI.(typeData)
+}
+
+type structDef struct {
+	Fields []namedType
+}
+
+func (x structDef) equals(yI equalsI) bool {
+	return tupleCmp(x.Fields, yI.(structDef).Fields)
+}
+
+type alias struct {
+	Target string
+}
+
+func (x alias) equals(yI equalsI) bool {
+	return x == yI.(alias)
+}
+
+type pkgData struct {
+	pkg     *types.Package
+	Funcs   map[string]funcData
+	Types   map[string]typeData
+	Structs map[string]structDef
+	Aliases map[string]alias
+}
+
+func newPkgData(pkg *types.Package) pkgData {
+	return pkgData{
+		pkg:     pkg,
+		Funcs:   make(map[string]funcData),
+		Types:   make(map[string]typeData),
+		Structs: make(map[string]structDef),
+		Aliases: make(map[string]alias),
+	}
+}
+
+type equalsI interface {
+	equals(equalsI) bool
+}
+
+func mapAnd[V equalsI](x, y map[string]V) map[string]V {
+	// assume the maps are equal at first, then check this assumption;
+	// first pass checks if they're equal and just returns x if so
+	// (maps are not modified after creation)
+	// if not equal, create new ANDed map
+
+	if len(x) == len(y) {
+		equal := true
+		for name, xV := range x {
+			if yV, ok := y[name]; !(ok && xV.equals(yV)) {
+				equal = false
+				break
+			}
+		}
+		if equal {
+			return x
+		}
+	}
+
+	out := make(map[string]V)
+	for name, xV := range x {
+		if yV, ok := y[name]; ok && xV.equals(yV) {
+			out[name] = xV
+		}
+	}
+	return out
+}
+
+// get pkgData with definitions existing in both x and y
+func (x pkgData) and(y pkgData) pkgData {
+	return pkgData{
+		Funcs:   mapAnd(x.Funcs, y.Funcs),
+		Types:   mapAnd(x.Types, y.Types),
+		Structs: mapAnd(x.Structs, y.Structs),
+		Aliases: mapAnd(x.Aliases, y.Aliases),
+	}
+}
+
+// also handle methods
+func parseFunc(obj *types.Func, pkg pkgData) {
+	signature := obj.Type().(*types.Signature)
+	// do not handle generic functions
+	if signature.TypeParams() != nil {
+		return
+	}
+}
+
+func parseType(obj *types.TypeName, pkg pkgData) {
+	if obj.IsAlias() {
+		pkg.Aliases[obj.Id()] = alias{Target: obj.Type().String()}
+		return
+	}
+	named := obj.Type().(*types.Named)
+	// do not handle generic types
+	if named.TypeParams() != nil {
+		return
+	}
+	if t, ok := named.Underlying().(*types.Struct); ok {
+		parseStruct(obj.Id(), t, pkg)
+	}
+
+	if false {
+		parseMethods(obj, pkg)
+	}
+}
+
+// false if method comes from embedded struct field
+func selfMethod(objT types.Type, f *types.Func) bool {
+	fmt.Println(f)
+	fmt.Println(f.Scope())
+	scope := f.Scope()
+	names := scope.Names()
+	if len(names) != 1 {
+		panic(objT)
+	}
+	realT := scope.Lookup(names[0]).(*types.Var).Type()
+
+	return objT == realT
+}
+
+func parseMethods(obj *types.TypeName, pgk pkgData) {
+	objT := obj.Type()
+	methods := types.NewMethodSet(objT)
+	numMethods := methods.Len()
+	methodNames := make(map[string]bool, numMethods)
+
+	for i := 0; i < numMethods; i++ {
+		method := methods.At(i).Obj()
+		methodNames[method.Name()] = true
+		if !selfMethod(objT, method.(*types.Func)) {
+			continue // comes from embedded field
+		}
+		// TODO
+		fmt.Printf("regular method %s\n", method.Name())
+	}
+
+	pObjT := types.NewPointer(objT)
+	pMethods := types.NewMethodSet(pObjT)
+	numPMethods := pMethods.Len()
+
+	for i := 0; i < numPMethods; i++ {
+		method := pMethods.At(i).Obj()
+		if methodNames[method.Name()] || !selfMethod(pObjT, method.(*types.Func)) {
+			continue // already handled or from embedded field
+		}
+		// TODO
+		fmt.Printf("pointer method %s\n", method.Name())
+	}
+
+	// TODO
+	if types.NewMethodSet(obj.Type()).Len() != types.NewMethodSet(types.NewPointer(obj.Type())).Len() {
+		fmt.Println(types.NewMethodSet(obj.Type()))
+		fmt.Println(types.NewMethodSet(types.NewPointer(obj.Type())))
+
+		idkF := types.NewMethodSet(obj.Type()).At(0)
+		//idkF := types.NewMethodSet(types.NewPointer(obj.Type())).At(0)
+		idkF2 := idkF.Obj().(*types.Func)
+		scope := idkF2.Scope()
+		names := scope.Names()
+		if len(names) != 1 {
+			panic(scope)
+		}
+		preT := scope.Lookup(names[0]).(*types.Var).Type()
+		var realT types.Type
+		if ptrT, ok := preT.(*types.Pointer); ok {
+			realT = ptrT.Elem()
+		} else {
+			realT = preT
+		}
+		fmt.Println(objT.String()) // os.file
+		fmt.Println(realT)         // os.File
+		fmt.Println(objT == realT)
+		fmt.Println(idkF.Obj().Name()) // close
+
+		panic(obj)
+	}
+}
+
+func parseStruct(name string, obj *types.Struct, pkg pkgData) {
+	numFields := obj.NumFields()
+	fields := make([]namedType, numFields)
+	for i := 0; i < numFields; i++ {
+		field := obj.Field(i)
+		fields[i] = namedType{
+			Name:     field.Name(),
+			DataType: field.Type().String(),
+		}
+	}
+	pkg.Structs[name] = structDef{Fields: fields}
+	fmt.Println(name, fields)
+}
+
 func walkPrint(ch <-chan string) {
 	fset := token.NewFileSet()
 	for path := range ch {
 		x := check1(parser.ParseDir(fset, path, nil, parser.PackageClauseOnly|parser.ParseComments))
 
-		for _, pkg := range x {
-			if strings.HasSuffix(pkg.Name, "_test") || pkg.Name == "main" || pkg.Name == "builtin" {
+		for _, astPkg := range x {
+			if strings.HasSuffix(astPkg.Name, "_test") || astPkg.Name == "builtin" { // || astPkg.Name == "main" {
 				continue
 			}
 
-			//ast.Print(fset, pkg)
+			//ast.Print(fset, astPkg)
 
-			if path == "image/gif" {
-				fileMap := filterPkg(pkg, path)
-				for _, pkg := range fileMap {
-					//fmt.Printf("		%s\n", arch)
+			if path == "os" {
+				pkgMap := filterPkg(astPkg, path)
+				var andedPkg pkgData
+				andedFirst := true
+				for pkgArch, pkg := range pkgMap {
+					fmt.Printf("		%s %s\n", path, pkgArch)
 					scope := pkg.Types.Scope()
 					names := scope.Names()
-					//fmt.Printf("%#v\n", names)
-					for _, name := range names {
-						obj := scope.Lookup(name)
+					fmt.Printf("%#v\n", names)
 
-						fmt.Printf("%s.%s: ", obj.Pkg().Path(), obj.Name())
+					pkgD := newPkgData(pkg.Types)
+					for _, name := range names {
+						switch obj := scope.Lookup(name).(type) {
+						case *types.Func:
+							fmt.Println(obj)
+							parseFunc(obj, pkgD)
+						case *types.TypeName:
+							fmt.Println(obj)
+							parseType(obj, pkgD)
+						}
+						continue
+
+						obj := scope.Lookup("a")
+
+						fmt.Printf("%s\n", obj)
+						//fmt.Printf("%s.%s: ", obj.Pkg().Path(), obj.Name())
+						fmt.Println(types.Id(obj.Pkg(), obj.Name()))
 
 						baseType := obj.Type()
 
-						switch t := baseType.Underlying().(type) {
+						//switch t := baseType.Underlying().(type) {
+						switch t := baseType.(type) {
 						case *types.Basic:
 							fmt.Printf("basic %s\n", t)
 						case *types.Struct:
 							fmt.Printf("struct %s\n", t)
 						case *types.Signature:
-							fmt.Printf("%s.%s: ", obj.Pkg().Path(), obj.Name())
-							fmt.Printf("func %s\n", t)
+							fmt.Printf("%s: func %s\n", obj.Id(), t)
 						case *types.Pointer:
 							fmt.Printf("ptr %s\n", t)
 						case *types.Interface:
@@ -363,6 +607,8 @@ func walkPrint(ch <-chan string) {
 							fmt.Printf("slice %s\n", t)
 						case *types.Chan:
 							fmt.Printf("chan %s\n", t)
+						case *types.Named:
+							fmt.Printf("named %s\n", t)
 						default:
 							panic(t)
 						}
@@ -376,6 +622,21 @@ func walkPrint(ch <-chan string) {
 						if numMethods > 0 {
 							fmt.Println()
 						}
+
+						methods = types.NewMethodSet(baseType)
+						numMethods = methods.Len()
+						for i := 0; i < numMethods; i++ {
+							fmt.Printf("method %d: %s\n", i, methods.At(i))
+						}
+						if numMethods > 0 {
+							fmt.Println()
+						}
+					}
+					if andedFirst {
+						andedPkg = pkgD
+						andedFirst = false
+					} else {
+						andedPkg = andedPkg.and(pkgD)
 					}
 				}
 			}
--- tests/main.go
+++ tests/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
+type base struct {
+	arr [2]int
+	i   int
+}
+
+//go:noinline
+func (b base) xyzzy() int {
+	return b.i
+}
+
+type container struct {
+	i int
+	base
+}
+
 // example from src/cmd/compile/abi-internal.md in golang/go
 //
 //go:noinline
@@ -15,4 +32,6 @@ func f(a1 uint8, a2 [2]uintptr, a3 uint8) (r1 struct {
 
 func main() {
 	f(3, [2]uintptr{7, 9}, 5)
+
+	fmt.Println(container{base: base{i: 5}}.xyzzy())
 }
